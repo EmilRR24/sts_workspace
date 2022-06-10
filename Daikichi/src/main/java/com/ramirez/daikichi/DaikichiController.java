4dfc8b32abdcbdc28238cc22a9b4d1db9944325f
EmilRR24 <grimlei2005@yahoo.com>
2022-06-10 10:48:21 -1000

    completed daikichi path routes

--- Daikichi/src/main/java/com/ramirez/daikichi/DaikichiController.java
+++ Daikichi/src/main/java/com/ramirez/daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.ramirez.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,16 @@ public class DaikichiController {
 	@RequestMapping("/daikichi/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	@RequestMapping("/daikichi/travel/{city}")
+	public String showCity(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	@RequestMapping("/daikichi/lotto/{number}")
+	public String showLotto(@PathVariable ("number") int number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 }
